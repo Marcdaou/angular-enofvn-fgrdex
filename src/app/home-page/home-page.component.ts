1cd8c370804140ede9a0cae078858b4aaa0533af
Marcdaou <marc7182@gmail.com>
2019-10-08 15:08:15 +0300

    2 files modified

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
